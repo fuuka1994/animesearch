9f92e2d63630ba106e52c81b82926e20afe95ab6
minhnh <minhnh.da@gmail.com>
2014-11-24 00:36:33 +0700

    Add 2 new function getAND and getNOT state of button

--- src/animesearch/view/AnimeFilter.java
+++ src/animesearch/view/AnimeFilter.java
@@ -12,7 +12,7 @@ import java.util.List;
 public class AnimeFilter extends JDialog {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private JPanel contentPane;
 	private JComboBox<String> jcbSeasonFrom;
 	private JComboBox<String> jcbSeasonTo;
@@ -20,14 +20,13 @@ public class AnimeFilter extends JDialog {
 	private JComboBox<String> jcbYearTo;
 	private JCheckBox chckbxAllTime;
 	private JButton btnOk;
-	
-	private static final String[] GENRES = {"Action","Adventure","Comedy","Drama","Ecchi","Fantasy",
-		"Harem","Horror","Martial Arts","Mecha","Music","Mystery","Magical Girl","Romance","Slice of Life",
-		"Sports","Tragedy","Historical"};
-	private static final String[] DEMOGRAPHICS = {"Shoujo", "Shounen", "Seinen", "Kodomo"};
-	
+
+	private static final String[] GENRES = { "Action", "Adventure", "Comedy", "Drama", "Ecchi", "Fantasy", "Harem", "Horror", "Martial Arts", "Mecha", "Music", "Mystery", "Magical Girl", "Romance", "Slice of Life", "Sports", "Tragedy", "Historical" };
+	private static final String[] DEMOGRAPHICS = { "Shoujo", "Shounen", "Seinen", "Kodomo" };
+
 	private List<ThreeStateButton> genreButtons;
 	private List<ThreeStateButton> demographicButtons;
+
 	/**
 	 * Launch the application.
 	 */
@@ -36,6 +35,15 @@ public class AnimeFilter extends JDialog {
 			public void run() {
 				try {
 					AnimeFilter frame = new AnimeFilter();
+					frame.setBtnOKActionListener(new ActionListener() {
+
+						@Override
+						public void actionPerformed(ActionEvent e) {
+							// TODO Auto-generated method stub
+							System.out.println("FUCKING PRINT THIS LINE");
+							System.out.println("AND: " + frame.getMustHaveGenreAND() + "\nNOT: " + frame.getExcludeGenreNOT());
+						}
+					});
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -57,72 +65,72 @@ public class AnimeFilter extends JDialog {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JLabel lblCategories = new JLabel("Categories");
 		lblCategories.setBounds(8, 11, 70, 14);
 		contentPane.add(lblCategories);
-		
+
 		JLabel lblSeason = new JLabel("Season");
 		lblSeason.setBounds(8, 224, 46, 14);
 		contentPane.add(lblSeason);
-		
+
 		chckbxAllTime = new JCheckBox("All time");
 		chckbxAllTime.setBounds(60, 220, 97, 23);
 		chckbxAllTime.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				changeComboBoxMode();
 			}
 		});
 		contentPane.add(chckbxAllTime);
-		
+
 		JPanel jpnSeason = new JPanel();
 		jpnSeason.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
 		jpnSeason.setBounds(10, 248, 268, 83);
 		contentPane.add(jpnSeason);
 		jpnSeason.setLayout(null);
-		
+
 		JLabel lblFrom = new JLabel("From");
 		lblFrom.setBounds(6, 19, 46, 14);
 		jpnSeason.add(lblFrom);
-		
+
 		JLabel lblTo = new JLabel("To");
 		lblTo.setBounds(6, 59, 46, 14);
 		jpnSeason.add(lblTo);
-		
+
 		jcbSeasonFrom = new JComboBox<String>();
 		jcbSeasonFrom.setBounds(62, 16, 100, 20);
 		jpnSeason.add(jcbSeasonFrom);
 		jcbSeasonFrom.setEditable(true);
-		
+
 		jcbSeasonTo = new JComboBox<String>();
 		jcbSeasonTo.setBounds(62, 56, 100, 20);
 		jpnSeason.add(jcbSeasonTo);
 		jcbSeasonTo.setEditable(true);
-		
+
 		jcbYearFrom = new JComboBox<String>();
 		jcbYearFrom.setBounds(202, 16, 60, 20);
 		jpnSeason.add(jcbYearFrom);
 		jcbYearFrom.setEditable(true);
-		
+
 		jcbYearTo = new JComboBox<String>();
 		jcbYearTo.setBounds(202, 56, 60, 20);
 		jpnSeason.add(jcbYearTo);
 		jcbYearTo.setEditable(true);
-		
+
 		JPanel jpnGenre = new JPanel();
 		jpnGenre.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Genre", TitledBorder.LEADING, TitledBorder.TOP, null, Color.GRAY));
 		jpnGenre.setBounds(8, 35, 349, 178);
 		contentPane.add(jpnGenre);
 		jpnGenre.setLayout(new GridLayout(0, 3));
-		
+
 		genreButtons = new ArrayList<ThreeStateButton>();
-		
+
 		for (int i = 0; i < GENRES.length; i++) {
 			final ThreeStateButton button = new ThreeStateButton();
 			button.addActionListener(new ActionListener() {
-				
+
 				@Override
 				public void actionPerformed(ActionEvent arg0) {
 					button.changeState();
@@ -140,17 +148,17 @@ public class AnimeFilter extends JDialog {
 		jpnDemographic.setBounds(368, 35, 109, 178);
 		contentPane.add(jpnDemographic);
 		jpnDemographic.setLayout(new GridLayout(0, 1));
-		
+
 		btnOk = new JButton("OK");
 		btnOk.setBounds(345, 280, 89, 23);
 		contentPane.add(btnOk);
-		
+
 		demographicButtons = new ArrayList<ThreeStateButton>();
-		
+
 		for (int i = 0; i < DEMOGRAPHICS.length; i++) {
 			final ThreeStateButton button = new ThreeStateButton();
 			button.addActionListener(new ActionListener() {
-				
+
 				@Override
 				public void actionPerformed(ActionEvent arg0) {
 					button.changeState();
@@ -162,11 +170,11 @@ public class AnimeFilter extends JDialog {
 			smallPanel.add(new JLabel(DEMOGRAPHICS[i], JLabel.LEFT), BorderLayout.CENTER);
 			jpnDemographic.add(smallPanel);
 		}
-		
+
 		init();
 	}
-	
-	private void init(){
+
+	private void init() {
 		if (jcbYearFrom != null) {
 			for (int i = 1991; i < 2014; i++) {
 				jcbYearFrom.addItem("" + i);
@@ -184,7 +192,7 @@ public class AnimeFilter extends JDialog {
 			jcbSeasonFrom.addItem("Fall");
 			jcbSeasonFrom.addItem("Winter");
 		}
-		
+
 		if (jcbSeasonTo != null) {
 			jcbSeasonTo.addItem("Spring");
 			jcbSeasonTo.addItem("Summer");
@@ -192,15 +200,15 @@ public class AnimeFilter extends JDialog {
 			jcbSeasonTo.addItem("Winter");
 		}
 	}
-	
-	private void changeComboBoxMode(){
+
+	private void changeComboBoxMode() {
 		jcbSeasonFrom.setEnabled(!chckbxAllTime.isSelected());
 		jcbSeasonTo.setEnabled(!chckbxAllTime.isSelected());
 		jcbYearFrom.setEnabled(!chckbxAllTime.isSelected());
 		jcbYearTo.setEnabled(!chckbxAllTime.isSelected());
 	}
-	
-	public String getAnimeFrom(){
+
+	public String getAnimeFrom() {
 		if ((jcbSeasonFrom != null) && (jcbYearFrom != null)) {
 			String season = (String) jcbSeasonFrom.getSelectedItem();
 			String year = (String) jcbYearFrom.getSelectedItem();
@@ -208,8 +216,8 @@ public class AnimeFilter extends JDialog {
 		}
 		return null;
 	}
-	
-	public String getAnimeTo(){
+
+	public String getAnimeTo() {
 		if ((jcbSeasonTo != null) && (jcbYearTo != null)) {
 			String season = (String) jcbSeasonTo.getSelectedItem();
 			String year = (String) jcbYearTo.getSelectedItem();
@@ -217,28 +225,56 @@ public class AnimeFilter extends JDialog {
 		}
 		return null;
 	}
-	
-	public boolean isAllTimeSelected(){
+
+	public boolean isAllTimeSelected() {
 		return chckbxAllTime.isSelected();
 	}
-	
-	public void setBtnOKActionListener(ActionListener listener){
+
+	public void setBtnOKActionListener(ActionListener listener) {
 		btnOk.addActionListener(listener);
 	}
-	
-	public List<Integer> getGenreValues(){
-		List<Integer> genreValues = new ArrayList<Integer>();
-		for (ThreeStateButton button : genreButtons) {
-			genreValues.add(button.getState());
+
+	public ArrayList<String> getMustHaveGenreAND() {
+		ArrayList<String> arrayList = new ArrayList<String>();
+
+		int i = 0;
+		for (ThreeStateButton threeStateButton : genreButtons) {
+			if (threeStateButton.getState() == ThreeStateButton.AND) {
+				arrayList.add(GENRES[i].toLowerCase());
+			}
+			i++;
 		}
-		return genreValues;
+
+		i = 0;
+		for (ThreeStateButton threeStateButton : demographicButtons) {
+			if (threeStateButton.getState() == ThreeStateButton.AND) {
+				arrayList.add(DEMOGRAPHICS[i].toLowerCase());
+			}
+			i++;
+		}
+
+		return arrayList;
 	}
-	
-	public List<Integer> getDemographicValues(){
-		List<Integer> demographicValues = new ArrayList<Integer>();
-		for (ThreeStateButton button : demographicButtons) {
-			demographicValues.add(button.getState());
+
+	public ArrayList<String> getExcludeGenreNOT() {
+		ArrayList<String> arrayList = new ArrayList<String>();
+
+		int i = 0;
+		for (ThreeStateButton threeStateButton : genreButtons) {
+			if (threeStateButton.getState() == ThreeStateButton.NOT) {
+				arrayList.add(GENRES[i].toLowerCase());
+			}
+			i++;
 		}
-		return demographicValues;
+
+		i = 0;
+		for (ThreeStateButton threeStateButton : demographicButtons) {
+			if (threeStateButton.getState() == ThreeStateButton.NOT) {
+				arrayList.add(DEMOGRAPHICS[i].toLowerCase());
+			}
+			i++;
+		}
+
+		return arrayList;
 	}
 }
